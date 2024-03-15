1ebed3ab71eddd8febd32d317610e3223140bfc7
hoseokjang <hoseok210@gmail.com>
2024-03-15 16:40:19 +0900

    init commit

---
+++ sample/src/main/java/com/example/sample/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "main";
+	}
+}
